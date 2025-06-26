5925593cdaba1350a296711870aabe508daf9f1e
itsjoacor <jdr123@gmail.com>
2025-06-26 11:37:30 -0300

    BossV1.2 - qfix restarPauseTimer

--- NewGmePlattformer/Menu/Pause Menu/pause_menu.gd
+++ NewGmePlattformer/Menu/Pause Menu/pause_menu.gd
@@ -8,10 +8,12 @@ func _on_resume_pressed():
 	resume_requested.emit()
 
 func _on_restart_pressed():
+	Global.elapsed_time = 0.0
+	Global.start_timer()
 	Global.gems_collected = 0  # Reset gems
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
 func _on_quit_pressed():
-	Global.gems_collected = 0  # Reset gems
+	Global.gems_collected = 0  # Reset gemsd
 	get_tree().quit()
