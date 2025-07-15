21eed53c48c50d27f665a528452836c7be0f5421
itsjoacor <jdr123@gmail.com>
2025-07-15 18:52:55 -0300

    Game_over_sound lack rules

--- NewGmePlattformer/DeathZone/death_zone.gd
+++ NewGmePlattformer/DeathZone/death_zone.gd
@@ -6,5 +6,4 @@ func _ready():
 	
 func _on_body_entered(body):
 	if body.name == "Player":
-		await get_tree().create_timer(0.2).timeout  
 		get_tree().change_scene_to_file("res://Menu/Game Over/geme_over.tscn")
--- NewGmePlattformer/Menu/Game Over/game_over.gd
+++ NewGmePlattformer/Menu/Game Over/game_over.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
+@export var lose_sound: AudioStream = preload("res://assets/game_over_sound.mp3")
+
 func _ready():
+	play_death_sound()
 	$Options/RetryButton.grab_focus()
 
 	await get_tree().process_frame
@@ -35,3 +38,13 @@ func _on_quit_pressed() -> void:
 func _on_menu_principal_pressed() -> void:
 	Global.gems_collected = 0 
 	get_tree().change_scene_to_file("res://Menu/Main Menu/main_menu.tscn")
+
+
+
+func play_death_sound():
+	# Access the node directly when needed
+	var sound_player = $DeathSoundPlayer
+	if lose_sound:
+		sound_player.stream = lose_sound
+	sound_player.play()
+	return sound_player  # Return the player so we can await it
